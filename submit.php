6bc77fc8587e4b31dbb96f16c89387813be658d4
Dmitrii Krasnoperov <d.krasnoperov@codedrop.cc>
2019-07-30 13:31:28 +0600

    Move block in submit.php

--- submit.php
+++ submit.php
@@ -5,27 +5,25 @@ $result = fopen("result.csv", "w+");
 $resultArray = addTableHeaders($indexsArray, 0);
 $line = 1;
 if (isset($_POST['mass'], $_POST['height'], $_POST['chest'])) {
-    $valid = validSubmit([(float)$_POST['mass'], (float)$_POST['height'], (float)$_POST['chest']]);
-}
-
-if ($valid) {
-    $resultArray[$line] = [
-        'mass' => (float)$_POST['mass'],
-        'height' => (float)$_POST['height'],
-        'chest' => (float)$_POST['chest']
-    ];
-
-    foreach ($indexsArray as $index) {
-        $resultArray = writeIndexToResultArray(
-            $index['name'],
-            $index['formula'](
-                (int)$_POST['height'],
+    if (validSubmit([(float)$_POST['mass'], (float)$_POST['height'], (float)$_POST['chest']])) {
+        $resultArray[$line] = [
+            'mass' => (float)$_POST['mass'],
+            'height' => (float)$_POST['height'],
+            'chest' => (float)$_POST['chest']
+        ];
+    
+        foreach ($indexsArray as $index) {
+            $resultArray = writeIndexToResultArray(
+                $index['name'],
+                $index['formula'](
+                    (int)$_POST['height'],
+                    (int)$_POST['mass'],
+                    (int)$_POST['chest']),
                 (int)$_POST['mass'],
-                (int)$_POST['chest']),
-            (int)$_POST['mass'],
-            $resultArray,
-            $line);
-    }
+                $resultArray,
+                $line);
+        }
+    }   
 }
 
 foreach ($resultArray as $resultArrayString) {
