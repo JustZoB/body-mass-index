f21e42265c163a389c745b7d556988f9757e7335
Dmitrii Krasnoperov <d.krasnoperov@codedrop.cc>
2019-08-08 13:52:36 +0600

    Move if up

--- submit.php
+++ submit.php
@@ -2,16 +2,14 @@
 require_once 'functions.php';
 require_once 'sqlFunctions.php';
 
-if (isset($_POST['mass'], $_POST['height'], $_POST['chest'])) {
-    if (validSubmit([(float)$_POST['mass'], (float)$_POST['height'], (float)$_POST['chest']])) {
-        $human[] = [
-            'mass' => (float)$_POST['mass'],
-            'height' => (float)$_POST['height'],
-            'chest' => (float)$_POST['chest']
-        ];
-        $array = getResult($human, true);
-        writeCsv($array);
-        sqlImportIndexs($array);
-        echo json_encode($array);
-    }
+if ((isset($_POST['mass'], $_POST['height'], $_POST['chest'])) && (validSubmit([$_POST['mass'], $_POST['height'], $_POST['chest']]))) {
+    $human[] = [
+        'mass' => (float)$_POST['mass'],
+        'height' => (float)$_POST['height'],
+        'chest' => (float)$_POST['chest']
+    ];
+    $array = getResult($human, true);
+    writeCsv($array);
+    sqlImportIndexs($array);
+    echo json_encode($array);
 }
